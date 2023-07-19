cadca404ceb25f39c6f2944cb600b43e04a431d8
ItsKayo <andrew_g98@yahoo.com>
2023-07-19 14:27:20 -0600

    Completed README and finished seeds

--- server/seeds/02_items.js
+++ server/seeds/02_items.js
@@ -6,8 +6,8 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('items').del()
   await knex('items').insert([
-    {user_id: 1, item_name: 'Apple', description: 'Honey crisp', quantity: 40},
-    {user_id: 1, item_name: 'Kiwi', description: 'Fuzzy, green, and yummy', quantity: 22},
-    {user_id: 3, item_name: 'Pear', description: 'Just a normal pear', quantity: 6},
+    {user_id: 1, item_name: 'Apple', description: `An apple is a round, edible fruit produced by an apple tree (Malus domestica). Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus. The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found. Apples have been grown for thousands of years in Asia and Europe and were introduced to North America by European colonists. Apples have religious and mythological significance in many cultures, including Norse, Greek, and European Christian tradition.`, quantity: 40},
+    {user_id: 1, item_name: 'Kiwi', description: `Kiwifruit (often shortened to kiwi in North American, British and continental European English) or Chinese gooseberry is the edible berry of several species of woody vines in the genus Actinidia.[1][2] The most common cultivar group of kiwifruit (Actinidia deliciosa 'Hayward')[3] is oval, about the size of a large hen's egg: 5–8 centimetres (2–3 inches) in length and 4.5–5.5 cm (1+3⁄4–2+1⁄4 in) in diameter. It has a thin, fuzzy, fibrous, tart but edible light brown skin and light green or golden flesh with rows of tiny, black, edible seeds. The fruit has a soft texture with a sweet and unique flavour.`, quantity: 22},
+    {user_id: 3, item_name: 'Pear', description: `Pears are fruits produced and consumed around the world, growing on a tree and harvested in late summer into mid-autumn. The pear tree and shrub are a species of genus Pyrus /ˈpaɪrəs/, in the family Rosaceae, bearing the pomaceous fruit of the same name. Several species of pears are valued for their edible fruit and juices, while others are cultivated as trees.`, quantity: 6},
   ]);
 };
